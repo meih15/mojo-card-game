3ec01c120d978e741e33061e7136e20584924a2c
Mei Huang <meihuang8328@gmail.com>
2024-05-03 19:55:09 -0700

    setup database connection using sequelize

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
 // add your database connection here
+const Sequelize = require('sequelize');
+const path = require('path')
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'database.sqlite')
+})
+
+module.exports = { sequelize }
